277218788db056e25da6d863fe0efa87e1cd1f24
n00bCod3r <n00bCod3r@protonmail.com>
2021-02-05 09:36:26 +0100

    Wrote menu HTML, added it to app root

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'MetroChat';
+  title = 'MetroChat'; //this will be replaced each time I use {{title}} in my pages
 }
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({ //this is the decoratoe of the component: it allows me to to add certain characteristics to my class
   selector: 'mc-menu', //this is the name that I will use in my pages to refer to this component: <mc-menu></mc-menu>
-  templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  templateUrl: './menu.component.html', //visual representation of the file: if I use template property I can directly write the html as the value: in this case it is recommended to surround the code with `` characters, to be able to use both ' and " inside it
+  styleUrls: ['./menu.component.css'] //if I need to style the component, I'd do it the file specified here
 })
 export class MenuComponent implements OnInit {
 
